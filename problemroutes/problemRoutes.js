acb47ee9a4b992734fea007144fa27291bd0eed8
Roshan Kosare <roshankosarepro@gmail.com>
2022-10-22 02:02:46 +0530

    added more problem routes to get specific problem

--- problemroutes/problemRoutes.js
+++ problemroutes/problemRoutes.js
@@ -5,21 +5,23 @@ const userProfileModel = require("../database/Models/userProfileModel");
 
 const problemRoutes = express.Router();
 
+
+// create new problem
 problemRoutes.post("/createproblem", authenticateUser, async (req, res) => {
   try {
     const problemTitle = req.body.problemTitle;
     const problemDes = req.body.problemDes;
-    const problemSampInput = req.body.probleSampleInput;
+    const problemSampleInput = req.body.problemSampleInput;
     const problemSampleOutput = req.body.problemSampleOutput;
     const problemTestCases = req.body.problemTestCases;
-    
+
     const auther = await userProfileModel.findById(req.user.userObjectId);
 
     const problem = new ProblemModel({
       auther: auther["_id"],
       title: problemTitle,
       problemDes: problemDes,
-      sampleInput: problemSampInput,
+      sampleInput: problemSampleInput,
       sampleOutput: problemSampleOutput,
       testCase: problemTestCases,
     });
@@ -32,13 +34,29 @@ problemRoutes.post("/createproblem", authenticateUser, async (req, res) => {
   }
 });
 
+// get problems
 problemRoutes.get("/getproblems", async (req, res) => {
   try {
     const problems = await ProblemModel.find()
       .select("_id title  auther ")
       .populate("auther", "username");
 
-      res.json({problems});
+    res.json({ problems });
+  } catch (err) {
+    res.json({ err });
+  }
+});
+
+//get single problem
+problemRoutes.get("/getproblem", async (req, res) => {
+  try {
+    const problemId = req.query.problemId;
+    const problem = await ProblemModel.findById(problemId).populate(
+      "auther",
+      "username _id "
+    );
+
+    res.json(problem);
   } catch (err) {
     res.json({ err });
   }
